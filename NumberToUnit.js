737fb88c95739137632b6c9335adcc6460ae1370
Santosh Nagargoje <34674840+santro93@users.noreply.github.com>
2022-06-07 16:18:23 +0530

    A number 1, 10, 100, 1000, etc and display unit, ten, hundred etc.

---
+++ NumberToUnit.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+
+console.log("Enter Numbers like 1,10,100,1000,100000");
+let number = 1000;
+console.log("Number is " + number);
+if (number == 1) {
+        console.log("Unit");
+}
+else if (number == 10) {
+        console.log("Ten");
+}
+else if (number == 100) {
+        console.log("Hundred");
+}
+else if (number == 1000) {
+        console.log("Thousand");
+}
+else if (number == 100000) {
+        console.log("Lakh");
+}
