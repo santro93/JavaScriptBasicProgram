c627d7a372a5097a412857696174e1b741f9ea73
santro93 <nsantosh435@gmail.com>
2022-06-07 11:37:37 +0530

    A coin flip and print out Heads or Tails accordingly.

---
+++ Head_Tail.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+var num = Math.floor(Math.random() * 2);
+console.log("Value of Random Number is "+num);
+
+if (num ==0) {
+	console.log("It's Tails");
+}
+else{
+	console.log("It's Heads");
+}
