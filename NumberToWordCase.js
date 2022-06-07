0899fa14190f3de7353ffa5241c50e780f0148c9
Santosh Nagargoje <34674840+santro93@users.noreply.github.com>
2022-06-07 16:30:32 +0530

    A single digit number and write the number in word using Case.

---
+++ NumberToWordCase.js
@@ -0,0 +1,37 @@
+// JavaScript source code
+// JavaScript source code
+var num = Math.floor(Math.random() * 10);
+console.log("Value of Random Number is " + num);
+
+
+switch (num) {
+    case 0:
+    console.log("Digit in Word is Zero");
+        break;
+    case 1:
+        console.log("Digit in Word is One");
+        break;
+    case 2:
+        console.log("Digit in Word is Two");
+        break;
+    case 3:
+        console.log("Digit in Word is Three");
+        break;
+    case 4:
+    console.log("Digit in Word is Four");
+        break;
+    case 5:
+    console.log("Digit in Word is Five");
+        break;
+    case 6:
+    console.log("Digit in Word is Six");
+        break;
+    case 7:
+    console.log("Digit in Word is Seven");
+    break;
+    case 8:
+    console.log("Digit in Word is Eight");
+    break;
+default :
+    console.log("Digit in Word is Nine");
+}
