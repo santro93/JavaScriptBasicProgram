29589645ea8a0207dd1a3dd49320004a13d4c62c
Santosh Nagargoje <34674840+santro93@users.noreply.github.com>
2022-06-07 17:00:43 +0530

    Number and Display the weekday Using Case.

---
+++ DigitToWeekdayUsingCase.js
@@ -0,0 +1,36 @@
+let num = Math.floor(Math.random() * 7 + 1);
+console.log("Entered Number for Weekday is : " + num);
+
+switch (num) {
+    case 1:
+        console.log("Digit in Word is Monday");
+        break;
+    case 2:
+        console.log("Digit in Word is Tuesday");
+        break;
+  case 3:
+        console.log("Digit in Word is Wednesday");
+        break;
+    case 4:
+    console.log("Digit in Word is Thursday");
+        break;
+    case 5:
+    console.log("Digit in Word is Friday");
+        break;
+    case 6:
+    console.log("Digit in Word is Saturday");
+        break;
+    case 7:
+        console.log("Digit in Word is Sunday");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
