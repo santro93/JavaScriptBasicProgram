32c62c989ab5a8761f710f3d2138940621aee6d4
Santosh Nagargoje <34674840+santro93@users.noreply.github.com>
2022-06-07 15:01:41 +0530

    Single Digit to Weekday.

---
+++ DigitToWeekday.js
@@ -0,0 +1,26 @@
+// JavaScript source code
+var num = Math.floor(Math.random() * 8 + 1);
+console.log("Value of Random Number is " + num);
+
+
+if (num == 1) {
+    console.log("Digit in Word is Monday");
+}
+else if (num == 2) {
+    console.log("Digit in Word is Tuesday");
+}
+else if (num == 3) {
+    console.log("Digit in Word is Wednesday");
+}
+else if (num == 4) {
+    console.log("Digit in Word is Thursday");
+}
+else if (num == 5) {
+    console.log("Digit in Word is Friday");
+}
+else if (num == 6) {
+    console.log("Digit in Word is Saturday");
+}
+else{
+    console.log("Digit in Word is Sunday");
+}
