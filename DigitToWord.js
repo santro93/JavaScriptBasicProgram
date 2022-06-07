56b71ad18cbc03a22d8221f51827e1bf2394ae33
Santosh Nagargoje <34674840+santro93@users.noreply.github.com>
2022-06-07 15:01:06 +0530

    Single Digit to Word.

---
+++ DigitToWord.js
@@ -0,0 +1,36 @@
+// JavaScript source code
+var num = Math.floor(Math.random() * 10);
+console.log("Value of Random Number is " + num);
+
+
+if (num == 0) {
+    console.log("Digit in Word is Zero");
+}
+else if (num == 1) {
+    console.log("Digit in Word is One");
+}
+else if (num == 2) {
+    console.log("Digit in Word is Two");
+}
+else if (num == 3) {
+    console.log("Digit in Word is Three");
+}
+else if (num == 4) {
+    console.log("Digit in Word is Four");
+}
+else if (num == 5) {
+    console.log("Digit in Word is Five");
+}
+else if (num == 6) {
+    console.log("Digit in Word is Six");
+}
+else if (num == 7) {
+    console.log("Digit in Word is Seven");
+}
+else if (num == 8) {
+    console.log("Digit in Word is Eight");
+}
+else{
+    console.log("Digit in Word is Nine");
+}
+
